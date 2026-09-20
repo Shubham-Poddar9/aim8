813f04ddbc0da18dd3422103e29e52b65d7817e1
Shubham-Poddar9 <poddarshubham258@gmail.com>
2026-09-20 13:59:44 +0545

    Add files via upload

---
+++ math.py
@@ -0,0 +1,70 @@
+import streamlit as st 
+import io
+from openai import OpenAI
+
+API_KEY="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"
+
+client=OpenAI(api_key=API_KEY,base_url=
+"https://api.groq.com/openai/v1")
+
+Model="openai/gpt-oss-20b"
+SYSTEM_PROMPT="""
+you are a math mastermind
+solve the problem step by step.
+Explain the reasoning clearly.
+give the final answer.
+verify thw answer if possible. """
+
+def solve_math(problem,level):
+    prompt=f"""
+{SYSTEM_PROMPT}
+level:{level}
+Problem:{problem}
+"""
+    try:
+        response=client.chat.completions.create(model=Model,messages=[{"role":"user","content":prompt}],temperature=0.1,max_tokens=1024)
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"Error{e}"
+
+st.set_page_config(page_title="math mastermind")
+st.title("math mastermind")
+st.write("Enter a math problem and get step by step solution")
+
+if "history" not in st.session_state:
+    st.session_state.history=[]
+
+problem=st.text_area(
+    "math problem",
+    placeholder="example: solve x*x +5x+6==0"
+
+)
+
+level=st.selectbox("level",["Basic","intermediate","advanced"])
+
+if st.button("solve"):
+    if not problem.strip():
+        st.warning("please enter a problem.")
+    else:
+        with st.spinner("Solving..."):
+            try:
+                answer= solve_math(problem,level)
+                st.session_state.history.append({
+                    "problem":problem,
+                    "answer":answer
+                })
+                st.subheader("solution")
+                st.write(answer)
+            except Exception as e:
+                st.error(f"error :{e}")
+
+if st.button("clear history "):
+    st.session_state.history=[]
+    st.rerun()
+
+if st.session_state.history:
+    st.subheader("History ")
+    for item in reversed(st.session_state.history):
+        st.write("question : ",item["problem"])
+        st.write("Answer",item["answer"])
+        st.divider()
